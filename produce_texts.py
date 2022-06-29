9f4905347f230b50c033399c7f6280041f42f695
Siva Kalyan <skalyan91@github.com>
2022-06-29 16:47:07 +1000

    Put together 1000 sentences from Wikipedia

--- produce_texts.py
+++ produce_texts.py
@@ -1,13 +1,18 @@
 import os
+from random import sample
 
 print("type the name of the file you wish to split by new lines into multiple files")
 fileName = input()
+print("enter the number of sentences you want")
+num = int(input())
 
 file = open(fileName)
+lines = file.readlines()
+lines = sample(lines, num)
 i = 0
 os.mkdir(fileName[:-4])
-while i <= 200:
+while i < num:
     directory = fileName[:-4] + "/" + str(i) + ".txt"
     current_file = open(directory, 'w')
-    current_file.write(file.readline())
+    current_file.write(lines[i])
     i += 1
