e86b33e92851b4b4eb4d1818aaac7dd4cac0e3e0
Siva Kalyan <skalyan91@github.com>
2022-06-29 22:08:48 +1000

    Trained a model, though it's probably replying on sentence length.

--- produce_texts.py
+++ produce_texts.py
@@ -1,5 +1,6 @@
 import os
 from random import sample
+from nltk import tokenize
 
 print("type the name of the file you wish to split by new lines into multiple files")
 fileName = input()
@@ -8,6 +9,7 @@ num = int(input())
 
 file = open(fileName)
 lines = file.readlines()
+lines = [l for l in lines if len(tokenize.word_tokenize(l)) > 10]
 lines = sample(lines, num)
 i = 0
 os.mkdir(fileName[:-4])
